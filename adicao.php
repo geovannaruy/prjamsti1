7acc4812e255d2485908f6de0cf30589a592d948
Glaubinho03 <85969179+juh22jjuh@users.noreply.github.com>
2022-10-06 10:58:58 -0300

    Update adicao.php

--- adicao.php
+++ adicao.php
@@ -4,11 +4,12 @@ $n1= $_POST['n1'];
 $n2= $_POST['n2'];
 
 $resultdo =$n1 =$n2;
-i( $resultado >20){
-    echo "A soma é : . " $resultado + 8;
+i( $resultado >27){
+    echo "A soma é : . " resultado54
+        echo "A soma é : . " $resultado + 8;
 
 }
 else{
-    echo "A soma é : . " $resultado  - 5;
+  - 5;
 }
 ?>
